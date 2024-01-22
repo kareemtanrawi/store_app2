5c928f2e767d5663716e4ab67fc4bf8e238788ad
Your Name <kareem01229526319@gmail.com>
2024-01-22 17:10:30 +0200

    add API class
    Signed-off-by: Your Name <kareem01229526319@gmail.com>

--- lib/services/category_services.dart
+++ lib/services/category_services.dart
@@ -1,24 +1,23 @@
 import 'dart:convert';
 
+import 'package:store_app2/helper/api.dart';
 import 'package:store_app2/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class CategoriesServices {
   Future<List<ProductModel>> getCategoriesProducts(
       {required String categoryName}) async {
-    http.Response response = await http.get(
-      Uri.parse('https://fakestoreapi.com/products/category/$categoryName'),
-    );
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      List<ProductModel> productsList = [];
+    // http.Response response = await http.get(
+    //   Uri.parse('https://fakestoreapi.com/products/category/$categoryName'),
+    // );
+    List<dynamic> data = await Api()
+        .get(url: 'https://fakestoreapi.com/products/category/$categoryName');
 
-      for (int i = 0; i < data.length; i++) {
-        productsList.add(ProductModel.fromJson(data[i]));
-      }
-      return productsList;
-    } else {
-      throw Exception('There is problem in status code ${response.statusCode}');
+    List<ProductModel> productsList = [];
+
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
     }
+    return productsList;
   }
 }
--- lib/services/get_all_product_services.dart
+++ lib/services/get_all_product_services.dart
@@ -1,23 +1,21 @@
 import 'dart:convert';
 
+import 'package:store_app2/helper/api.dart';
 import 'package:store_app2/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
 class AllProductServices {
   Future<List<ProductModel>> getAllProduct() async {
-    http.Response response =
-        await http.get(Uri.parse('https://fakestoreapi.com/products'));
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+    // http.Response response =
+    //     await http.get(Uri.parse('https://fakestoreapi.com/products'));
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products');
 
-      List<ProductModel> productsList = [];
-      for (int i = 0; i < data.length; i++) {
-        productsList.add(ProductModel.fromJson(data[i]));
-      }
-
-      return productsList;
-    } else {
-      throw Exception('There is problem in status code ${response.statusCode}');
+    List<ProductModel> productsList = [];
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
     }
+
+    return productsList;
   }
 }
